a36a811fce57a34a5cfd89b1bc4c14e3865ff68b
Samuel Bentos <samuelbentosgomez@gmail.com>
2021-06-05 21:23:38 -0300

    algoritmo js simple entregable

---
+++ samuelBentos.js
@@ -0,0 +1,5 @@
+let entrada1 = prompt("Ingrese su nombre");
+let entrada2 = prompt("Ingrese su apellido");
+let entrada3 = prompt("Ingrese su correo");
+let salida = " Nombre: " + entrada1 + " Apellido: " + entrada2 + " Correo: " + entrada3;
+alert(salida)
